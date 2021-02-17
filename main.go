c00d787d258ca441246c548b3ad6500d6154e4a9
sivchari <shibuuuu5@gmail.com>
2021-02-18 02:06:52 +0900

    add gentype and go generate cmd

--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/sivchari/dogen/dogen"
 )
 
 func main() {
 	log.SetFlags(0)
-	err := dogen.Run(os.Args, os.Stdout, os.Stderr)
-	if err != nil && err != flag.ErrHelp && err != dogen.ShowVersion {
-		log.Println(err)
-		os.Exit(2)
+	if err := dogen.Run(os.Args, os.Stdout, os.Stderr); err != nil && err != flag.ErrHelp && err != dogen.ShowVersion {
+		log.Fatal(err)
+	}
+	if err := exec.Command("go", "generate", "./...").Run(); err != nil {
+		log.Fatal(err)
 	}
 	log.Println(os.Args[0] + " ʕ◔ϖ◔ʔ ")
-	os.Exit(1)
 }
